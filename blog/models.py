190d62187c7b0295f72df70667204eafb04b91ef
Abhay Agarwal <abhayagarwal482@gmail.com>
2020-07-06 17:05:54 +0530

    Add Home page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class blog(models.Model):
+    tital = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/')
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+
